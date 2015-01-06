7878ffaee4f0d93d6893f1b678337deff32cd813
Matt Warren <matt.warren@live.co.uk>
2015-01-06 22:49:40 +0000

    Make site compatible with WebSites
    Make the site work on Azure, pull question data from Blob storage on
    start-up

--- WebSite/Global.asax.cs
+++ WebSite/Global.asax.cs
@@ -1,6 +1,13 @@
-﻿using StackOverflowTagServer;
+﻿using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using StackOverflowTagServer;
 using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
 using System.Threading;
+using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -12,8 +19,7 @@ namespace Server
     {
         internal static Lazy<TagServer> TagServer = new Lazy<TagServer>(() =>
             {
-                var filename = @"C:\Users\warma11\Downloads\__Code__\StackOverflowTagServer-master\Questions.bin";
-                return StackOverflowTagServer.TagServer.CreateFromFile(filename);
+                return CreateTagServer();
             }, LazyThreadSafetyMode.ExecutionAndPublication);
 
         protected void Application_Start()
@@ -25,7 +31,125 @@ namespace Server
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             // For initialisation to happen here, rather than waiting for the first request
-            var temp = WebApiApplication.TagServer.Value;
+            try
+            {
+                var temp = WebApiApplication.TagServer.Value;
+            }
+            catch (Exception ex)
+            {
+                Trace.Write(ex);
+            }
+        }
+
+        private static TagServer CreateTagServer()
+        {
+            var dataFolder = HttpContext.Current.Server.MapPath("~/Data");
+            Trace.WriteLine("Data folder: " + dataFolder);
+            ListFolderInfo(dataFolder);
+
+            var questionsPath = Path.Combine(dataFolder, "Questions-NEW.bin");
+            if (File.Exists(questionsPath) == false)
+            {
+                if (Directory.Exists(dataFolder) == false)
+                    Directory.CreateDirectory(dataFolder);
+
+                Trace.WriteLine("StorageConnectionString: " + CloudConfigurationManager.GetSetting("StorageConnectionString"));
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                    CloudConfigurationManager.GetSetting("StorageConnectionString"));
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                CloudBlobContainer container = blobClient.GetContainerReference("data");
+                ListBlobsInContainer(container);
+
+                foreach (IListBlobItem item in container.ListBlobs(null, false))
+                {
+                    //if (item.GetType() == typeof(CloudBlockBlob) && item.Uri.ToString().EndsWith(".zip"))
+                    if (item.GetType() == typeof(CloudBlockBlob))
+                    {
+                        CloudBlockBlob blob = (CloudBlockBlob)item;
+                        //Trace.WriteLine(string.Format("Found blob {0}", blob.Name));
+
+                        //if (blob.Name != "intermediate-AnswerCount.zip")
+                        //if (blob.Name != "Questions-NEW.zip")
+                        if (blob.Name != "Questions-NEW.bin")
+                            continue;
+                        Trace.WriteLine(string.Format("Found blob {0}", blob.Name));
+
+                        var blobOutput = Path.Combine(dataFolder, blob.Name);
+                        DownloadBlob(blob, blobOutput);
+
+                        //var decompressedFile = Path.Combine(dataFolder, Path.GetFileNameWithoutExtension(blobOutput) + ".bin");
+                        //if (File.Exists(decompressedFile))
+                        //    File.Delete(decompressedFile);
+                        //var decompressTimer = Stopwatch.StartNew();
+                        //ZipFile.ExtractToDirectory(blobOutput, dataFolder);
+                        //decompressTimer.Stop();
+                        //Trace.WriteLine(string.Format("Took {0} to decompress to {1} ({2})", 
+                        //                decompressTimer.Elapsed, Path.GetFileName(blobOutput), Path.GetFileName(decompressedFile)));
+                        //var info = new FileInfo(decompressedFile);
+                        //Trace.WriteLine(string.Format("Decompressed file {0}, {1:N0} bytes", Path.GetFileName(decompressedFile), new FileInfo(decompressedFile).Length));
+                    }
+                }
+            }
+
+            ListFolderInfo(dataFolder);
+
+            return StackOverflowTagServer.TagServer.CreateFromFile(questionsPath);
+        }
+
+        private static void DownloadBlob(CloudBlockBlob blob, string outputPath, bool deleteIfAlreadyExists = false)
+        {
+            // Save blob contents to a file.
+            if (deleteIfAlreadyExists && File.Exists(outputPath))
+                File.Delete(outputPath);
+
+            using (var fileStream = File.OpenWrite(outputPath))
+            {
+                var timer = Stopwatch.StartNew();
+                blob.DownloadToStream(fileStream);
+                timer.Stop();
+                Trace.WriteLine(string.Format("Took {0} to download {1:N0} bytes", timer.Elapsed, blob.Properties.Length));
+                Trace.WriteLine(string.Format("File {0}, Info (on disk) {1:N0} bytes", Path.GetFileName(outputPath), new FileInfo(outputPath).Length));
+            }
+        }
+
+       
+
+        private static void ListFolderInfo(string folderName)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.Arguments = "/c dir " + folderName;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.Start();
+
+            p.WaitForExit();
+            string output = p.StandardOutput.ReadToEnd();
+            //p.WaitForExit();
+            Trace.WriteLine(output);
+        }
+
+        private static void ListBlobsInContainer(CloudBlobContainer container)
+        {
+            // Loop over items within the container and output the length and URI.
+            foreach (IListBlobItem item in container.ListBlobs(null, false))
+            {
+                if (item.GetType() == typeof(CloudBlockBlob))
+                {
+                    CloudBlockBlob blob = (CloudBlockBlob)item;
+                    Trace.WriteLine(string.Format("Block blob of length {0}: {1}", blob.Properties.Length, blob.Uri));
+                }
+                else if (item.GetType() == typeof(CloudPageBlob))
+                {
+                    CloudPageBlob pageBlob = (CloudPageBlob)item;
+                    Trace.WriteLine(string.Format("Page blob of length {0}: {1}", pageBlob.Properties.Length, pageBlob.Uri));
+                }
+                else if (item.GetType() == typeof(CloudBlobDirectory))
+                {
+                    CloudBlobDirectory directory = (CloudBlobDirectory)item;
+                    Trace.WriteLine(string.Format("Directory: {0}", directory.Uri));
+                }
+            }
         }
     }
 }
