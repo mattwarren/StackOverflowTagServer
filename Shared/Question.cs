8c967c90ad693446291d1a065ea53720968bb5a0
Matt Warren <matt.warren@live.co.uk>
2015-08-13 10:23:56 +0100

    Added a bit more functionality to the Question class

--- Shared/Question.cs
+++ Shared/Question.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using ProtoBuf;
+using System.Linq;
 
 namespace Shared
 {
@@ -21,7 +22,12 @@ namespace Shared
             get
             {
                 if (tagList == null)
-                    tagList =  new List<string>(RawTags.Split(new string[] { " ", "<", ">" }, StringSplitOptions.RemoveEmptyEntries));
+                {
+                    tagList = new List<string>(
+                                    RawTags.Split(new string[] { " ", "<", ">" }, StringSplitOptions.RemoveEmptyEntries)
+                                           .Select(s => String.Intern(s)));
+                }
+
                 return tagList;
             }
             set
@@ -49,8 +55,8 @@ namespace Shared
         {
             return base.ToString();
 
-            // Print the full Question for each result (this is harder to read!!
-            //Console.WriteLine("\t{0}", string.Join("\n\t", result.Select(r => new
+            // Print the full Question for each result (this is harder to read!!)
+            //Logger.Log("\t{0}", string.Join("\n\t", result.Select(r => new
             //                                                    {
             //                                                        r.Id,
             //                                                        Tags = string.Join(", ", r.Tags),
@@ -60,6 +66,42 @@ namespace Shared
             //                                                        r.ViewCount,
             //                                                        r.AnswerCount
             //                                                    })));
+        }
+
+        public override bool Equals(object obj)
+        {
+            // If parameter is null return false.
+            if (obj == null)
+            {
+                return false;
+            }
+
+            // If parameter cannot be cast to Question return false.
+            Question other = obj as Question;
+            if (other == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return this.Equals(other);
+        }
+
+        public bool Equals(Question other)
+        {
+            // If parameter is null return false:
+            if (other == null)
+            {
+                return false;
+            }
+
+            return Id == other.Id;
+        }
+
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
         }
     }
 }
